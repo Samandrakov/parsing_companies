fc5c3ea3741c0a2c95fc4ef59975b25245fe2e87
Danila <127055128+Samandrakov@users.noreply.github.com>
2025-02-24 21:55:00 +0300

    fixed pagingation

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
-my_dict = {
-    "i": 1,
-    "ii": 2,
-    "iii": 3
-}
+from urllib.parse import urlparse
 
-number = int(input("Введите число от 1 до 3: "))
-if 1 <= int(number) <= 3:
-    for key, val in my_dict.items():
-        if val == number:
-            if key:
-                print(f"Ключ для числа {number}: {key}")
-else:
-    print("Пожалуйста, введите число в диапазоне от 1 до 3.")
+def extract_language_subdomain(url):
+    parsed_url = urlparse(url)
+    netloc = parsed_url.netloc
+    parts = netloc.split('.')
+
+    if len(parts) > 2 and len(parts[0]) in (2, 3):
+        return parts[0]
+    else:
+        return None
+
+
+url = "https://ru.wikipedia.org/wiki/%D0%9F%D1%91%D1%82%D1%80%20III"
+language_subdomain = extract_language_subdomain(url)
+print(language_subdomain)
